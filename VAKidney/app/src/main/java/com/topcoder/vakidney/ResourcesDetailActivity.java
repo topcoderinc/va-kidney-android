1b5d6f337f616f38b54fe918073c3d7630618a56
Gaurav <gaurav.kheterpal@gmail.com>
2018-09-12 13:13:08 +0530

    Fixed compilation error

--- VAKidney/app/src/main/java/com/topcoder/vakidney/ResourcesDetailActivity.java
+++ VAKidney/app/src/main/java/com/topcoder/vakidney/ResourcesDetailActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.topcoder.vakidney.databinding.ActivityResourcesDetailBinding;
 
